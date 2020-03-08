d313b02fab3d5bf0bcaa31c16ce332b410391d20
Richard Bobo <richard.bobo@gmail.com>
2020-03-08 22:37:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,13 @@
 # require modules here
 
-def load_library
-  # code goes here
+require 'yaml'
+
+
+ #translation = YAML.load_file('emoticons.yml')
+
+def load_library(link) 
+translation = YAML.load_file(link)
+ translation 
 end
 
 def get_japanese_emoticon
